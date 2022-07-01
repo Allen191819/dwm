280fe0a5911dbb1c59908c68de6415f6f89268ac
Allen.gitee <8676187+allengiteexyy@user.noreply.gitee.com>
2022-07-01 10:06:38 +0800

    update

--- dwm.c
+++ dwm.c
@@ -3174,7 +3174,7 @@ int
 main(int argc, char *argv[])
 {
     if (argc == 2 && !strcmp("-v", argv[1]))
-        die("dwm-"VERSION);
+        die("dwm-",VERSION);
     else if (argc != 1)
         die("usage: dwm [-v]");
     if (!setlocale(LC_CTYPE, "") || !XSupportsLocale())
